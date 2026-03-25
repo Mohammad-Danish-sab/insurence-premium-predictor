d9d9ddab4662edb58f68d3252d6fc9124c3bb194
Mohammad Danish <danish.sab05@gmail.com>
2026-03-25 15:03:46 +0530

    add databse

---
+++ backend/app/database.py
@@ -0,0 +1,14 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.config import settings
+
+client = None
+db = None
+
+async def connect_db():
+    global client, db
+    client = AsyncIOMotorClient(settings.MONGO_URI)
+    db = client[settings.DB_NAME]
+    print("✅ MongoDB connected")
+
+def get_db():
+    return db
