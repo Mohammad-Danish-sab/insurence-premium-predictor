86528ea5fe2daa792e3cb885ab4a86a21d377a64
Mohammad Danish <danish.sab05@gmail.com>
2026-03-27 23:36:41 +0530

    set the env and the config file

---
+++ backend/app/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    MONGO_URI: str
+    DB_NAME: str = "insurance-premium"
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
