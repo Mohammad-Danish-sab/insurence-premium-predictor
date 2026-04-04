086a87283700ec8ef6e654331428e83e9a4b3875
Mohammad Danish <danish.sab05@gmail.com>
2026-04-04 22:06:48 +0530

    fix the main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth, predict, history, admin
-from app.database import connect_db
+from app.database import connect_db, disconnect_db
+from slowapi.errors import RateLimitExceeded
+from contextlib import asynccontextmanager
+from app.config import settings
+from app.middleware.rate_limiter import limiter, rate_limit_exceeded_handler
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await connect_db()       # ← runs on startup
+    yield
+    await disconnect_db() 
+
 
 app = FastAPI(
     title="Insurance Premium Prediction API",
@@ -29,6 +41,16 @@ app.include_router(predict.router, prefix="/api/predict", tags=["Prediction"])
 app.include_router(history.router, prefix="/api/history", tags=["History"])
 app.include_router(admin.router,   prefix="/api/admin",   tags=["Admin"])
 
-@app.get("/")
-def root():
-    return {"message": "Insurance API is running ✅"}
+@app.get("/", tags=[" Root"])
+async def root():
+    return {
+        "app":     settings.APP_NAME,
+        "version": "1.0.0",
+        "status":  "running ",
+        "docs":    "/docs",
+        "redoc":   "/redoc"
+    }
+
+@app.get("/health", tags=["Root"])
+async def health_check():
+    return {"status": "healthy"}
