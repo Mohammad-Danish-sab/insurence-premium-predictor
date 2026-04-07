9c51226ce9467eb27abafbda6222d264d5cdb5f0
Mohammad Danish <danish.sab05@gmail.com>
2026-04-08 02:27:15 +0530

    feat: login

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -4,3 +4,7 @@ export const signup = async (data) => {
   const res = await api.post("/api/auth/signup", data);
   return res.data;
 };
+export const login = async (data) => {
+  const res = await api.post("/api/auth/login", data);
+  return res.data;
+};
