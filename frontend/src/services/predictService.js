183c94603494b316e4d3a3d87b670d7e20e6e910
Mohammad Danish <danish.sab05@gmail.com>
2026-04-09 00:02:19 +0530

    feat:fix the routes

--- frontend/src/services/predictService.js
+++ frontend/src/services/predictService.js
@@ -9,3 +9,8 @@ export const predictGuest = async (data) => {
   const res = await api.post("/api/predict/guest", data);
   return res.data;
 };
+
+export const whatIfSimulator = async (data) => {
+  const res = await api.post("/api/predict/what-if", data);
+  return res.data;
+};
