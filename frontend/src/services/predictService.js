db926233afab392dba9ed1a9578bfabdb486ab0b
Mohammad Danish <danish.sab05@gmail.com>
2026-04-08 20:07:20 +0530

    feat: predict guest

--- frontend/src/services/predictService.js
+++ frontend/src/services/predictService.js
@@ -4,3 +4,8 @@ export const predictPremium = async (data) => {
   const res = await api.post("/api/predict/", data);
   return res.data;
 };
+
+export const predictGuest = async (data) => {
+  const res = await api.post("/api/predict/guest", data);
+  return res.data;
+};
