2a747f77e3d472dcc172c1be2187843bc3680c60
Mohammad Danish <danish.sab05@gmail.com>
2026-04-11 01:24:54 +0530

    feat:add auth button and avatar button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -33,6 +33,7 @@ export default function Navbar() {
         <nav className="bg-white shadow-sm sticky top-0 z-50">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center h-16">
+
               <Link to="/" className="flex items-center gap-2">
                 <Shield className="text-secondary w-7 h-7" />
                 <span className="text-xl font-bold text-primary">
@@ -64,7 +65,26 @@ export default function Navbar() {
                 )}
               </div>
 
-              
+         <div className="hidden md:flex items-center gap-4">
+            {user ? (
+              <div className="relative">
+
+                  <button
+                  onClick={() => setDropOpen(!dropOpen)}
+                  className="flex items-center gap-2 bg-gray-100 hover:bg-gray-200
+                             px-3 py-2 rounded-full transition"
+                >
+                  <div className="w-8 h-8 rounded-full bg-secondary flex
+                                  items-center justify-center text-white font-bold text-sm">
+                    {user.full_name?.charAt(0).toUpperCase()}
+                  </div>
+                  <span className="text-sm font-medium text-gray-700">
+                    {user.full_name?.split(" ")[0]}
+                  </span>
+                </button>
+
+            </div>
+            ):
             </div>
           </div>
         </nav>
