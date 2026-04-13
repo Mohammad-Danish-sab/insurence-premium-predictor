7dd847393c024ea37b3a3d2a579c980b7ff294cf
Mohammad Danish <danish.sab05@gmail.com>
2026-04-13 21:23:17 +0530

    feat:add home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import {
+  Shield,
+  Zap,
+  FileText,
+  BarChart2,
+  CheckCircle,
+  ArrowRight,
+  Star,
+} from "lucide-react";
