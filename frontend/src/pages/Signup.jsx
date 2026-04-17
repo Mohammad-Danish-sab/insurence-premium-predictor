3eaa189d7b8684eb255d50353bcd9c1cf36e5d6b
Mohammad Danish <danish.sab05@gmail.com>
2026-04-17 17:19:25 +0530

    set the function

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -4,3 +4,11 @@ import { useAuth } from "../context/AuthContext";
 import { signup as signupService } from "../services/authService";
 import { validateSignup } from "../utils/validateForm";
 import { Shield, Eye, EyeOff, Loader, CheckCircle } from "lucide-react";
+
+
+const handleChange = (e) => {
+    SVGAnimateTransformElement({ ...form, [e.target.name]: e.target.value })
+    setErrors({ ...errors, [e.target.name]: ""})
+    setApiError("")
+}
+
