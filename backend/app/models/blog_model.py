43af6b21f20178977c2aba3f75f90df645fd973b
Mohammad Danish <danish.sab05@gmail.com>
2026-05-10 02:14:11 +0530

    feat: add blog model

---
+++ backend/app/models/blog_model.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+
+def blog_structure(data):
+
+    return {
+
+        "title": data.title,
+
+        "content": data.content,
+
+        "image": data.image,
+
+        "author": data.author,
+
+        "created_at": datetime.utcnow()
+    }
