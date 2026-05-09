12d83970ff4654417f99f0f398af32766ad31047
Mohammad Danish <danish.sab05@gmail.com>
2026-05-10 02:40:50 +0530

    feat:add get single blog

--- backend/app/routes/blogs.py
+++ backend/app/routes/blogs.py
@@ -31,3 +31,35 @@ async def create_blog(
 
         "blog_id": str(result.inserted_id)
     }
+
+@router.get("/blogs/{blog_id}")
+async def get_single_blog(blog_id: str):
+
+    db = get_db()
+
+    try:
+
+        blog = await db.blogs.find_one(
+            {
+                "_id": ObjectId(blog_id)
+            }
+        )
+
+        if not blog:
+
+            raise HTTPException(
+                status_code=404,
+                detail="Blog not found"
+            )
+
+        blog["_id"] = str(blog["_id"])
+
+        return blog
+
+    except Exception:
+
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid blog ID"
+        )
+    
