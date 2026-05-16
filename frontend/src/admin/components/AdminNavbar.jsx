a6b57850b352a67d10687df0d230f73221827f85
Mohammad Danish <danish.sab05@gmail.com>
2026-05-17 03:25:25 +0530

    feat:add adminnavbar pages

---
+++ frontend/src/admin/components/AdminNavbar.jsx
@@ -0,0 +1,15 @@
+function AdminNavbar() {
+  return (
+    <div className="bg-white shadow-sm p-4 flex justify-between items-center">
+      <h2 className="text-2xl font-bold">Insurance Admin Dashboard</h2>
+
+      <div className="flex items-center gap-3">
+        <div className="w-10 h-10 rounded-full bg-black text-white flex items-center justify-center">
+          A
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdminNavbar;
